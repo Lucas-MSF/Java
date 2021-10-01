1fe5792b55879be1a8135e9d0b4872dd0fa8d95d
Lucas Macena <lucasmasafi@gmail.com>
2021-10-01 20:51:07 -0300

    Atividades-Algoritimos-Em-Java

--- Algoritimos-Java/atividade-1/questao2.java
+++ Algoritimos-Java/atividade-1/questao2.java
@@ -1,23 +1,44 @@
 package atividade;
-
 import java.util.Scanner;
 
-public class questao2{
-	public static void main(String[] args) {
+public class questao2 {
 
-		Scanner ent = new Scanner(System.in);
-		int numeros[]= new int[20];
-	
-		for(int i=0;i<20;i++) {
-			numeros[i]= ent.nextInt();
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int vet[]=new int[20];
+		int aux=0;
+		int vetorOrganizado[]=new int[20];
+		System.out.println("Insira 20 numeros no Vetor:");
+		for (int i=0;i<20;i++)
+			vet[i]= sc.nextInt();
+		
+		System.out.println("vetor digitado:");
+		for (int i=0;i<20;i++)
+			System.out.print(vet[i]+" ");
+
+	//Organizando o vetor inserido para mostrar os valores inseridos em forma decrescente
+		
+		vetorOrganizado=vet;
+		
+		for (int i=0;i<20;i++) {
+			for (int j=0;j<20;j++) {
+				if(vetorOrganizado[i]<vetorOrganizado[j]) {
+					aux=vetorOrganizado[i];
+					vetorOrganizado[i]= vetorOrganizado[j];
+					vetorOrganizado[j]= aux;
+				}
+			}
 		}
-		System.out.println("Crescente --------------");
-		for(int i=0;i<20;i++) {
-			System.out.println(numeros[i]);
-		}
-		System.out.println("Decrescente --------------");
-		for(int i=19;i>=0;i--) {
-			System.out.println(numeros[i]);
-		}
+				
+		
+		//valores para teste: 1 4 5 6 7 8 9 20 18 19 17 16 2 3 10 12 11 13 14 15
+		
+		
+			
+		System.out.println("\nvetor digitado em ordem decrescente:");
+		for(int i=19;i>=0;i--) 
+				System.out.print(vetorOrganizado[i]+" ");
+
 	}
+
 }
