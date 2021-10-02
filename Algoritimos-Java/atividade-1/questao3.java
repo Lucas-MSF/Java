1a13b9c58f066cf873a1979c5cda2ecdf7a9de37
Lucas Macena <lucasmasafi@gmail.com>
2021-10-01 21:38:33 -0300

    Atividades-Algoritimos-Em-Java

--- Algoritimos-Java/atividade-1/questao3.java
+++ Algoritimos-Java/atividade-1/questao3.java
@@ -3,16 +3,14 @@ package atividade;
 public class questao3 {
 
 	public static void main(String[] args) {
-		int contador = 0;
-		double graos = 1;
-		double soma = 0;
-		do {
-		contador = contador + 1;
-		if( contador > 1) 
-		graos = graos * 2;
-		soma = soma + graos;
-		}while(contador <64);
-		System.out.println("A quantidade de grãos paga será de: " + soma );
+	double graos=1, soma=1;
+	
+		
+		for(int i=1;i<64;i++) {
+			graos*=2;
+			soma+=graos;
+		}
+		System.out.print("Soma dos graos: "+soma);
 	}
 
 }
