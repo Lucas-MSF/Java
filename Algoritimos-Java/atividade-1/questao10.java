9cf6c5059aa3d130634ce2902df9886912b5ec56
Lucas Macena <lucasmasafi@gmail.com>
2021-10-02 13:40:54 -0300

    Atividades-Algoritimos-Em-Java

--- Algoritimos-Java/atividade-1/questao10.java
+++ Algoritimos-Java/atividade-1/questao10.java
@@ -6,43 +6,34 @@ public class questao10 {
 
 	public static void main(String[] args) {
 		
-		Scanner ent= new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);
 		
-		int[] numeros= new int[6];
-		int[] pares= new int[6];
-		int[] impar= new int[6];
-		int aux=0;
-		int soma= 0;
-		int aux2= 0;
+		int numeros[]=new int[6];
+		String pares="";
+		String impares="";
+		int soma=0, contador=0;
 		
 		
-		for(int i=0; i<6;i++) {
-			numeros[i]= ent.nextInt();
-			if(numeros[i] % 2 ==0) {
-				pares[aux]= numeros[i];
-				aux++;
+		System.out.print("Insira 6 numeros: ");
+		for (int i=0;i<6;i++)
+			numeros[i]= sc.nextInt();
+		
+		for (int i=0;i<6;i++) {
+			if(numeros[i]%2==0) {
+				soma+=numeros[i];
+				pares+= numeros[i]+" ";
+			}else {
+				contador++;
+				impares+= numeros[i]+" ";
 			}
-			else {
-				impar[aux2]= numeros[i];
-				aux2++;
-			}
+				
 		}
+		System.out.println("Numeros pares: "+ pares);
+		System.out.println("Soma dos numeros pares: "+ soma);
+		System.out.println("Numeros impares: "+ impares);
+		System.out.println("Quantidade de numeros impares: "+contador);
+		sc.close();
 		
-		System.out.print("Os numeros pares são: ");
-		
-		for(int i=0; i<aux;i++) {
-			System.out.print(" " + pares[i]);
-			soma= pares[i]+soma;
-		}
-		
-		System.out.println( "");
-		System.out.println( "A soma deles é: " + soma);
-		System.out.print("Os numeros impares são: ");
-		for(int i=0; i<aux2;i++) {
-			System.out.print(" " + impar[i]);
-		}
-		System.out.println( "");
-		System.out.println( "A quantidade de numeros impares é: " + aux2);
 	}
 
 }
--- Algoritimos-Java/atividade-1/questao9.java
+++ Algoritimos-Java/atividade-1/questao9.java
@@ -3,22 +3,16 @@ package atividade;
 public class questao9 {
 
 	public static void main(String[] args) {
-		int aux = 1001;
-		int contador = 0;
-		int valor = 0;
 		
-		for(int i = 1000; i < aux ; i++) {
-			if(i%11 == 5) {
-				contador ++;
-				aux+=1;
-			}else {
-				aux +=1;
-			}
-			if(contador == 5) {
-				valor = i;
-				break;
-			}
+		int x=1001;
+		int contador=0;
+		
+		while(contador <5) {
+			if(x%11==5)
+				contador++;
+			x++;
 		}
-		System.out.println("O numero " + valor + " foi encontrado");
+		System.out.println(x);
 	}
+
 }
