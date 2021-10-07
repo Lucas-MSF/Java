86d56f371730e08bc38dbf8017f3714f497b407a
Lucas Macena <lucasmasafi@gmail.com>
2021-10-07 14:12:18 -0300

    Sistema-de-compras-sem-POO

---
+++ Algoritimos-Java/sistemas-compras/SistemaCompra.java
@@ -0,0 +1,149 @@
+package Atividade2;
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class SistemaCompra {
+
+	static public Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		Locale.setDefault(Locale.US);
+
+		String clientes[] = { "", "", "", "", "", "", "", "", "", "" };
+		String produtos[] = { "Feijao", "Acucar", "Arroz", "Macarrao", "Café" };
+		float precos[] = { (float) 4.50, (float) 3.50, (float) 6.00, (float) 3.25, (float) 4.25 };
+		int quantidade[] = { 50, 40, 55, 35, 20 };
+		float caixa = 0;
+		int opcao, opcao_venda, quantidade_venda;
+
+		do {
+			limparTela();
+			menu();
+
+			opcao = sc.nextInt();
+
+			switch (opcao) {
+			case 1:
+				mostrarEstoque(produtos, precos, quantidade);
+				pause();
+				break;
+			case 2:
+				mostrarCliente(clientes);
+				pause();
+				break;
+			case 3:
+				for (int i = 0; i < clientes.length; i++) {
+					if (clientes[i] == "") {
+						clientes[i] = cadastrarCliente();
+						break;
+					}
+				}
+				break;
+			case 4:
+				System.out.println("Valor no caixa: " + caixa);
+				pause();
+				break;
+			case 5:
+				System.out.print("Digite o ID do produto que deseja comprar: ");
+				opcao_venda = sc.nextInt() - 1;
+				if (opcao_venda >= 0 && opcao_venda <= 5) {
+					System.out.print("Informe a quantidade: ");
+					quantidade_venda = sc.nextInt();
+					if (quantidade_venda > 0 && quantidade_venda <= quantidade[opcao_venda]) {
+						recibo(produtos[opcao_venda], precos[opcao_venda], quantidade_venda);
+					} else {
+						System.out.println("Quantidade Invalida!");
+						pause();
+					}
+				} else {
+					System.out.println("Opcao Invalida!");
+					pause();
+				}
+
+				break;
+			case 6:
+				limparTela();
+				break;
+			case 7:
+				System.out.println("Encerrando sistema...");
+				break;
+			default:
+				System.out.println("Opcao Invalida!");
+				pause();
+				break;
+			}
+
+		} while (opcao != 7);
+		sc.close();
+	}
+
+	public static void menu() {
+		System.out.println("- - - - - - - M E N U - - - - - - -");
+		System.out.println("1- Mostrar produtos em estoque");
+		System.out.println("2- Mostrar clientes");
+		System.out.println("3- Cadastrar novo cliente");
+		System.out.println("4- Mostrar dinheiro em caixa");
+		System.out.println("5- Vender produto");
+		System.out.println("6- Limpar tela");
+		System.out.println("7- Sair");
+		System.out.println("- - - - - - - - - - - - - - - - - -");
+		System.out.print(">> ");
+	}
+
+	public static void mostrarEstoque(String produtos[], float precos[], int quantidade[]) {
+		for (int i = 0; i < produtos.length; i++)
+			System.out.printf("id %d- Produto: %s - Preco: %.2f - Quantidade em estoque: %d\n", i + 1, produtos[i],
+					precos[i], quantidade[i]);
+		System.out.println("");
+
+	}
+
+	public static void mostrarCliente(String clientes[]) {
+		for (int i = 0; i < clientes.length; i++)
+			System.out.printf("ID %d: %s\n", i + 1, clientes[i]);
+	}
+
+	public static String cadastrarCliente() {
+		String nome = "";
+		System.out.print("Insira o nome do cliente que deseja inserir:");
+		sc.nextLine();
+		nome = sc.nextLine();
+		return nome;
+	}
+
+	public static void recibo(String produto, float preco, int quantidade_venda)
+			throws IOException, InterruptedException {
+		limparTela();
+		DateTimeFormatter data_venda = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+		DateTimeFormatter hora_venda = DateTimeFormatter.ofPattern("HH:mm");
+		System.out.println("---------------R E C I B O---------------");
+		System.out.println("Produto: " + produto);
+		System.out.println("Quantidade: " + quantidade_venda);
+		System.out.printf("Valor por unidade: %.2f \n", preco);
+		System.out.printf("Total: %.2f \n", preco * quantidade_venda);
+		System.out.println("Data: " + data_venda.format(LocalDateTime.now()));
+		System.out.println("Hora: " + hora_venda.format(LocalDateTime.now()));
+		System.out.println("------------------------------------------");
+	}
+
+	public static void limparTela() throws IOException, InterruptedException {
+		if (System.getProperty("os.name").contains("Windows"))
+			new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+		else
+			Runtime.getRuntime().exec("clear");
+	}
+
+	public static String pause() {
+
+		System.out.println("Sistema pausado, digite qualquer coisa para continuar: ");
+		String pause;
+		pause = sc.next();
+		return pause;
+	}
+
+}
